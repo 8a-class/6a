0561288d8910f512a10a45d4ed3067743b136712
8a-class <95986875+8a-class@users.noreply.github.com>
2022-01-15 17:36:59 +0300

    Add files via upload

---
+++ scriptschedule.js
@@ -0,0 +1,66 @@
+// var pon = document.getElementById='pon';
+// var vtor = getElementById='vtor';
+// var sred = getElementById='sred';
+// var chet = getElementById='chet';
+// var pyat = getElementById='pyat';
+// var subb = getElementById='subb';
+// var vosc = getElementById='vosc';
+
+
+// function pon () {
+
+// document.getElementById('pon').style.color = "green"
+
+// }
+
+
+function pass () {
+
+
+var pass = prompt("Введите Пароль!")
+
+
+if (pass == "shkarol") {
+
+alert("Готово!");
+location="https://000webhost.com/"
+
+	}else {
+
+alert("Неверный Пароль!")
+
+	}
+}
+
+
+var info = document.getElementById="text"
+
+
+function i () {
+
+if (info.style.display=="inline"){
+
+info.style.display="block"
+
+}else {
+
+info.style.display="inline"
+
+}
+
+}
+
+
+function i () {
+
+if (confirm("Вы Хотите посмотреть дз?")) {
+
+location="https://net.citycheb.ru/angular/school/studentdiary/"
+
+}else {
+
+alert="Дейстиве Отмененно"
+
+}
+
+}
